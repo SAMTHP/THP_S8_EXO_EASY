875c6957af6d151ed5f00568744f1257546b7d2f
SAMTHP <samir_615@live.fr>
2018-11-19 23:30:34 +0100

    MAJ

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// ************** Afficher la valeur max **************
+numbers = [1,2,3,4,5];
+
+console.log("La valeur max est le chiffre : "+Math.max(...numbers)); // Affiche la valeur max de l'array numbers
+
+// ************** Compter les voyelles d'un string **************
+
 var sentence = "J'ai des voyelles en masse !" // Déclaration et affectation de la variable sentence
 
 var i = 0;
@@ -10,13 +17,15 @@ function countVowel(str){ // Création de la fonction counVowel()
 		if(spliter == "a" || spliter == "e" || spliter == "i" || spliter == "o" || spliter == "u"){
 			vowel.push(spliter) // Stock toutes les voyelles si la dondition est remplie
 		}
-	}console.log("Le nombre de voyelles est : "+vowel.length)+".";
+	}console.log("Le nombre de voyelles est : "+vowel.length);
 }
 
 countVowel(sentence); // Execution de la fonction avec initialisation de la variable sentence en tant que paramètre
 
+// ************** Inverser les éléments du string **************
+
 function ReverseStr(str){ // Création de la fonction qui inversera les caractères de la phrase
-	console.log(str.split('').reverse().join(''));
+	console.log("Résultat de l'inversion : "+str.split('').reverse().join(''));
 }
 
 ReverseStr(sentence); // Execution de la fonction avec initialisation de la variable sentence en tant que paramètre
